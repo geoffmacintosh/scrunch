27e2a6fb48272e66aa0366bc0e794251dda4cea5
Geoff MacIntosh <geoff@mac.into.sh>
2016-12-19 21:07:37 -0700

    Stop rewriting filename

--- scrunch.rb
+++ scrunch.rb
@@ -22,12 +22,8 @@ def crush_file(input_file, output_file)
   "afconvert \"#{input_file}\" -f m4bf -d aacp 32000 -o \"#{output_file}\""
 end
 
-def make_filename(input_file, metadata)
-  if /\,.*\([0-9]{4}\)/.match(input_file)
-   /.*(?=\.[mM]4[aAbB])/.match(input_file).to_s + "-new.m4b"
-  else
-    "#{metadata["ART"]} #{metadata["nam"]}.m4b"
-  end
+def make_filename(input_file)
+  input_file[0...-4] + "-new.m4b"
 end
 
 def apply_metadata(file, metadata, cover)
@@ -66,15 +62,14 @@ input_filename = ARGV[0]
 
 if File.file?(input_filename) then
   input_filename = File.absolute_path(input_filename)
-  
+
   metadata = get_metadata(input_filename)
-  filename = File.join(File.dirname(input_filename),
-                       make_filename(input_filename, metadata))
+  output_filename = make_filename(input_filename)
   cover = get_cover(input_filename)
 
-  puts "scrunching..."
-  system crush_file(input_filename, filename)
-  system apply_metadata(filename, metadata, cover)
+  system "scrunching..."
+  system crush_file(input_filename, output_filename)
+  system apply_metadata(output_filename, metadata, cover)
   system "rm \"#{cover}\""
 else
   puts "No such file or directory"
